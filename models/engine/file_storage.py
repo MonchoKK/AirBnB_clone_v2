fe70a1b1d1fae277c19d3ab4827b3ee07ffac070
Kabelo <kabelokamogelo96@gmail.com>
2024-07-29 11:31:16 -0700

    models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+classes = {
+    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+    'State': State, 'City': City, 'Amenity': Amenity,
+    'Review': Review
+}
 
 
 class FileStorage:
@@ -8,13 +22,40 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        # Return only instances of cls if provided
+        if cls and cls in classes.keys() or cls in classes.values():
+            # if cls and cls in classes.keys():
+            if cls in classes.values():
+                cls = cls.__name__  # so that we are dealing with a string
+            objs = {}
+            # reload objects
+            curr_objs = self.all()
+            if curr_objs:
+                for key, val in curr_objs.items():
+                    if key[:key.find('.')] == cls:
+                        objs[key] = val
+                return objs
+        # else, Return all objects
         return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
+    def delete(self, obj=None):
+        """Delete object"""
+        # check if object exists and delete it if it does
+        if obj:
+            # self.reload()  # reload current objects
+            if self.all():
+                objs_keys = self.all().keys()
+                this_key = obj.to_dict()['__class__'] + '.' + obj.id
+                if this_key in objs_keys:
+                    del (self.all()[this_key])
+                # save objects after deletion
+                self.save()
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -27,24 +68,15 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """ calls the reload() method to deserialize JSON file """
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+
+from sqlalchemy import MetaData, Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state',
+                          cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(self):
+        """ cities getter for fileStorage option
+        Returns all cities whose state_id == current state's id (self.id) """
+        # get all City objects and filter w.r.t state_id
+        from models import storage
+        all_cities = storage.all(City)  # returns a dict of objects
+        # state_cities = {}
+        state_cities = []
+        if all_cities:
+            for key, val in all_cities.items():
+                if self.id in str(val):
+                    # state_cities[key] = str(val)
+                    state_cities.append(val)
+                    # state_cities.append(str(val))
+        return state_cities  # a dict of City objects in this state
