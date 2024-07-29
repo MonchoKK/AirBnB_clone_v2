e5da3d31bdb10983478822e58ea3e35738e931ee
Kabelo <kabelokamogelo96@gmail.com>
2024-07-29 11:12:33 -0700

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
-""" Starts a Flask application that MUST be lisening on 0.0.0.0, port 5000.
-    Required
-        routes: /:display "Hello HBNB!", /hbnb: display "HBNB" &
-                /c/<text>: display "C" forllowed by 'text' variable (with any
-                underscores replaced by a space)
-        MUST use the option 'strict_slashes=False in route definition
+"""Starts a Flask web application:
+- The web application must be listening on 0.0.0.0, port 5000
+- Routes:
+  - /: display "Hello HBNB!"
+  - /hbnb: display "HBNB"
+  - /c/<text>: display "C " followed by the value of the text variable
+    (replace underscore _ symbols with spaces)
+- You must use the option strict_slashes=False in your route definition
 """
-from flask import Flask, escape
 
-# instantiate a Flask application
+from flask import Flask
+from werkzeug.utils import escape
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False  # override default globally
+app.url_map.strict_slashes = False  # Override default globally
 
 
-# define a route to trigger the function defined right after
 @app.route('/')
-def hello_world():
-    """ Returns 'Hello HBNB' """
+def hello_hbnb():
+    """Returns 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb')
-def hello_world_2():
-    """ Returns 'HBNB' """
+def hbnb():
+    """Returns 'HBNB'"""
     return "HBNB"
 
 
-# the route captures a value (text) from the URL & passes to the function
 @app.route('/c/<text>')
-def hello_world_3(text):
-    """ Returns 'C' followed by (space replaced underscores) text """
-    return "C {}".format(escape(text).replace('_', ' '))
+def c_text(text):
+    """Returns 'C ' followed by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return "C {}".format(escape(text))
 
 
 if __name__ == '__main__':
-    # if run as an application (not module), listen on all public IPs
     app.run(host='0.0.0.0', port=5000)
